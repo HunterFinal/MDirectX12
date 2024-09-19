0c860103418f95b27f16a5591c2435cb6ba55d56
HunterFinal <schiffenficken@gmail.com>
2024-09-19 14:55:53 +0900

    create solution

---
+++ main.cpp
@@ -0,0 +1,21 @@
+/*
+
+MRenderFramework
+Author : MAI ZHICONG
+
+Description : RenderFramework used by Game (Graphics API: DirectX12)
+
+Update History: 2024/09/19 Create
+
+Version : alpha_1.0.0
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Hello world" << endl;
+    return 0;
+}
