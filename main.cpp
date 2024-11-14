f741da2095397945784e800b7ea18895887a6d3b
麦 <139850260+HunterFinal@users.noreply.github.com>
2024-11-14 18:53:22 +0900

    back up

--- main.cpp
+++ main.cpp
@@ -15,6 +15,7 @@ Version : alpha_1.0.0
 // 代わりに、DXIL 経由でシェーダー モデル 6 を使用してください。 GitHub を参照してください。
 // url https://github.com/microsoft/DirectXShaderCompiler
 
+#include <memory>
 // Debug include
 #ifdef _DEBUG
 
@@ -28,85 +29,16 @@ Version : alpha_1.0.0
 #include <Interfaces/IWindowInfo.h>
 // end MWindow include
 
-// DX12 include
-#include <d3d12.h>
-#include <dxgi1_6.h>
-
-#pragma comment(lib, "d3d12.lib")
-#pragma comment(lib, "dxgi.lib")
-
-#include <DirectXMath.h>
-
-// end DX12 include
-
-// shader compiler include
-#include <d3dcompiler.h>
-
-#pragma comment(lib, "d3dcompiler.lib")
-
-// end shader compiler include
-
-// DirectXTex lib include
-
-#include <DirectXTex.h>
-
-#pragma comment(lib, "DirectXTex.lib")
-
-// end DirectXTex lib include
-
-#include <vector>
-#include <string>
-
-#pragma region DX12 Wrapper
-
-#include <Graphics_DX12/DX12Device.h>
-#include <Graphics_DX12/DX12DXGIFactory.h>
-#include <Graphics_DX12/CommandList.h>
-#include <Graphics_DX12/CommandQueue.h>
-#include <Graphics_DX12/RootSignature.h>
-#include <Graphics_DX12/PipelineState.h>
-#include <Graphics_DX12/DX12SwapChain.h>
-#include <Graphics_DX12/Fence.h>
-
-#include <Graphics_DX12/VertexBufferContainer.h>
-#include <Graphics_DX12/IndexBufferContainer.h>
-
-#include <Graphics_DX12/DescriptorHandle.h>
-#include <Graphics_DX12/DescriptorHeap.h>
-#include <Graphics_DX12/ConstantBuffer.h>
-
-#include <Graphics_DX12/RenderTarget.h>
-
-#include <Graphics_DX12/ShaderResBlob.h>
-
+// DX12 Graphics
+#pragma region DX12 Graphics
 #include <Graphics_DX12/GraphicsSystem.h>
-
 #pragma endregion
-// end region of DX12 Wrapper
-
-#include <Graphics_DX12/Texture.h>
-#include <RandomGenerator.hpp>
-
-struct Vertex
-{
-    DirectX::XMFLOAT3 pos;
-    DirectX::XMFLOAT2 uv;
-};
-
-/// @brief
-/// アライメントに揃えたサイズを返す
-/// @param size 元のサイズ
-/// @param alignment アラインメントサイズ
-/// @return アラインメントを揃えたサイズ
-size_t AlignmentedSize(size_t size, size_t alignment)
-{
-  return size + alignment - size % alignment;
-}
+// end DX12 Graphics
 
 #ifdef _DEBUG
 int main()
 #else
-int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
+int WINAPI WinMain(_In_ HINSTANCE,HINSTANCE,LPSTR,int)
 #endif
 {
     // Window
@@ -116,13 +48,9 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
     {
         return -1;
     }
-
-    UINT frame = 0;
-
     MSG msg = {};
 
-
-    IGraphics* g = new MFramework::GraphicsSystem();
+    std::shared_ptr<IGraphics> g = std::make_shared<MFramework::GraphicsSystem>();
     g->Init(test.GetHWND(), true);
 
     #pragma region Main Loop
@@ -135,7 +63,7 @@ int WINAPI WinMain(HINSTANCE,HINSTANCE,LPSTR,int)
     } 
    
     g->Terminate();
-    delete g;
+    g.reset();
 
     return 0;
 }
